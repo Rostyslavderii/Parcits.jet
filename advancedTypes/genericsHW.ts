38106ea5be5d73ee39cbb45de3f4e433897cad62
Rostyslavderii <105818935+Rostyslavderii@users.noreply.github.com>
2023-02-19 02:49:52 +0200

    generics for function of objects

--- advancedTypes/genericsHW.ts
+++ advancedTypes/genericsHW.ts
@@ -1,13 +1,76 @@
-function getPromise () {
+type ComplexType = string | number; 
+
+
+
+
+function getPromise(): Promise<Array<string | number>> {
   return new Promise((resolve) => {
-    resolve(['Text', 50]);
-  });
-}
+    if (typeof resolve === 'string') {
+      resolve(['Text', 50]);
+    }
+})
+};
+
 
 getPromise ()
 .then((data) => {
   console.log(data);
 });
+
+type resolve = {
+  text: string;
+  number: number;
+}
+
+interface Page {
+  title: string;
+  annotation: string;
+  numberPage: number;
+}
+
+const pageAnnotation: Pick<Page, 'annotation' | 'numberPage'> = {
+  annotation: 'Small page',
+  numberPage: 1,
+};
+
+// Есть функция, она принимает два аргумента, в один попадает
+//  name и color в другую часть position и weight. Нужно явно указать, что эти поля из AllType.
+// И сама функция возвращает AllType.Воспользуйтесь Pick.
+
+const pickAll1: Pick<AllType, 'name' | 'color'> = {
+  name:'alex',
+color: 'green',
+}
+
+
+const pickAll2: Pick<AllType, 'position' | 'weight'> ={
+  position:50,
+weight: 50,
+}
+
+
+function propType<pickAll1, pickAll2>(carl: pickAll1, son: pickAll2) {
+  
+  return {
+    name: pickAll1.name, 
+    color: pickAll1.color,
+    position: pickAll2.position,
+    weight: pickAll2.weight,
+
+}
+
+
+
+}
+
+type AllType =  { 
+  name: string; 
+  position: number;
+  color: string;
+  weight: number;
+}
+
+
 
 function compare (top, bottom): AllType {
   return {
@@ -19,9 +82,14 @@ function compare (top, bottom): AllType {
 }
 
 
-function merge (objA, objB) {
+// Укажите дженерики для функции объединения объектов.
+
+
+function merge<Y extends object, Z extends object >(objA: Z, objB: Y) {
   return Object.assign(objA, objB);
 }
+
+const merged = merge({name: 'Alisa'}, {age: 28});
 
 
 
@@ -36,3 +104,19 @@ class Page extends Component {
     console.log(this.props.title);
   }
 }
+
+// У вас есть следующие классы
+
+class Component {
+  constructor (public props:T) {
+
+  }
+}
+
+class Page extends Component {
+  pageInfo () {
+    console.log(this.props.title);
+  }
+}
+
+// Только добавляя дженерики для классов и интерфейс уберите ошибку.
